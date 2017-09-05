83e2761b94594f9eb55af1f98c84cff0eacf7bcf
TheStrix <parthbhatia98@gmail.com>
2017-09-05 07:01:44 +0000

    Settings: Add back Liquid Device Maintainers
    Change-Id: I54dd1cdaf14c1a2c84a688cc066079d256078be3
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- src/com/android/settings/deviceinfo/DeviceInfoSettings.java
+++ src/com/android/settings/deviceinfo/DeviceInfoSettings.java
@@ -30,6 +30,7 @@ import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.dashboard.SummaryLoader;
 import com.android.settings.deviceinfo.firmwareversion.FirmwareVersionPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
+import com.android.settings.deviceinfo.DeviceMaintainerPreference;
 import com.android.settings.deviceinfo.LiquidVersionPreferenceController;
 import com.android.settings.deviceinfo.simstatus.SimStatusPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -141,6 +142,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(
                 new BuildNumberPreferenceController(context, activity, fragment, lifecycle));
+        controllers.add(new DeviceMaintainerPreference(context));
         return controllers;
     }
 
