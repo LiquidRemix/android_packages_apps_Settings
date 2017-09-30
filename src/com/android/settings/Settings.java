0a33617e44de1640dd51761d69a284f4494ac144
maxwen <github.com>
2017-09-30 06:04:57 +0200

    Add liquid lounge
    settings: temp liquid lounge icon
    
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,5 +177,8 @@ public class Settings extends SettingsActivity {
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
+	
+	// LiquidRemix Settings
+    public static class LiquidLoungeSettingsActivity extends SettingsActivity {}
 
 }
