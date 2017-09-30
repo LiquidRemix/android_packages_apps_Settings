8504a97cc1d97731d016b322c77439b56835dc02
maxwen <github.com>
2017-09-30 06:04:57 +0200

    settings: add liquid lounge
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -36,6 +36,8 @@ import com.android.settings.security.LockscreenDashboardFragment;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.liquid.liquidlounge.LiquidLoungeSettings;
+
 import java.util.Map;
 
 /**
@@ -89,6 +91,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_NOTIFICATIONS);
         PARENT_TO_CATEGORY_KEY_MAP.put(LockscreenDashboardFragment.class.getName(),
                 CategoryKey.CATEGORY_SECURITY_LOCKSCREEN);
+		PARENT_TO_CATEGORY_KEY_MAP.put(LiquidLoungeSettings.class.getName(),
+                CategoryKey.CATEGORY_LIQUID);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
