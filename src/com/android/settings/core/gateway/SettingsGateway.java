6088e4754c25cc613e60453bcdac3fde51cdceee
BeansTown106 <nbenis106@gmail.com>
2017-09-30 10:42:18 +0800

    add activity to launch headsup settings fragment
    Change-Id: I6587a1bf7e132e10759b5a9d858ddfd3ed866b22

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -129,6 +129,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.liquid.liquidlounge.LiquidLoungeSettings;
+import com.liquid.liquidlounge.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -249,6 +250,7 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
 			LiquidLoungeSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
