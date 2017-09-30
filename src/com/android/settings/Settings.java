57e37f678f38fe0d1f0c9dd0eb4d5eda72dbd55f
maxwen <github.com>
2017-09-30 06:04:57 +0200

    settings: add liquid lounge
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -191,4 +191,6 @@ public class Settings extends SettingsActivity {
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+	// LiquidRemix settings
+    public static class LiquidLoungeSettingsActivity extends SettingsActivity {}
 }
