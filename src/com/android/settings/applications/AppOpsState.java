OctoDev-Project-Legacy/octogen_packages_apps_Settings

AppOps: use arrays and strings without summaries
Octogenify

Change-Id: I63d16aa9b56907b18ed1196f8d76274ebb6bc077
Signed-off-by: XrTV <binder_z@mail.ru>

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_labels_octogen);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_octogen);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_octogen);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());

LiquidRemix/android_packages_apps_Settings

src/com/android/settings/applications/AppOpsState.java
@@ -56,8 +56,8 @@ public class AppOpsState {
         mContext = context;
         mAppOps = (AppOpsManager)context.getSystemService(Context.APP_OPS_SERVICE);
         mPm = context.getPackageManager();
-        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_summaries);
-        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels);
+        mOpSummaries = context.getResources().getTextArray(R.array.app_ops_labels_liquid);
+        mOpLabels = context.getResources().getTextArray(R.array.app_ops_labels_liquid);
     }
 
     public static class OpsTemplate implements Parcelable {

src/com/android/settings/applications/AppOpsSummary.java
@@ -107,7 +107,7 @@ public class AppOpsSummary extends InstrumentedPreferenceFragment {
         mContentContainer = container;
         mRootView = rootView;
 
-        mPageNames = getResources().getTextArray(R.array.app_ops_categories);
+        mPageNames = getResources().getTextArray(R.array.app_ops_categories_liquid);
 
         mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
         MyPagerAdapter adapter = new MyPagerAdapter(getChildFragmentManager());