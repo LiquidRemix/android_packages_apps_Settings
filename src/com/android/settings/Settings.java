5ca7cb0160c995d33bacbba23ce54e3c8b916e57
BeansTown106 <nbenis106@gmail.com>
2018-09-15 12:35:15 +0000

    add activity to launch headsup settings fragment
    Signed-off-by: VenkatVishalV <venkatvishal124@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -164,6 +164,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class HeadsUpSettingsActivity extends SettingsActivity { /* empty */ }
 
 	// LiquidRemix settings
     public static class LiquidLoungeSettingsActivity extends SettingsActivity {}
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.liquid.liquidlounge.fragments.HeadsUpSettings;
 import com.liquid.liquidlounge.LiquidLoungeSettings;
 
 public class SettingsGateway {
@@ -264,6 +265,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             LiquidLoungeSettings.class.getName(),
+            HeadsUpSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
