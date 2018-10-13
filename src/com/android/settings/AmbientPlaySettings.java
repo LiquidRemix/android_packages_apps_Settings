18b7a46d6ecfe702cf47786dbe7dc8944f6a8e14
spezi77 <spezi7713@gmx.net>
2018-10-13 16:29:33 +0530

    Ambient Play: Allow to configure the scan interval [2/2]

--- src/com/android/settings/AmbientPlaySettings.java
+++ src/com/android/settings/AmbientPlaySettings.java
@@ -22,6 +22,7 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.liquid.AmbientPlayPreferenceController;
 import com.android.settings.liquid.AmbientPlayKeyguardPreferenceController;
+import com.android.settings.liquid.AmbientPlayIntervalPreferenceController;
 
 public class AmbientPlaySettings extends DashboardFragment {
     private static final String TAG = "AmbientPlaySettings";
@@ -31,6 +32,7 @@ public class AmbientPlaySettings extends DashboardFragment {
         super.onAttach(context);
         addPreferenceController(new AmbientPlayPreferenceController(context));
         addPreferenceController(new AmbientPlayKeyguardPreferenceController(context));
+        addPreferenceController(new AmbientPlayIntervalPreferenceController(context));
     }
 
     @Override
